e10c90f57b8f7fcd0b4b1cfe673630c0065ff79e
hawryyy30 <harrystream86@gmail.com>
2023-12-10 00:57:38 +0700

    Initialized codebase

---
+++ app/src/main/java/com/foranger/sumeruimpact/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.foranger.sumeruimpact
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
